6e926226e20e7227e6da2b258c9fed15a3f84534
Atsuya Kumano <at.kumano@gmail.com>
2017-09-27 20:33:27 -0700

    model ready. data insert done

--- metha_to_postgres.py
+++ metha_to_postgres.py
@@ -1,50 +1,122 @@
 from app import db
-from models import Article
+from models import Article, Author, Category, article_author, article_category
 
-import gzip, glob
+import os, gzip, glob
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
-def xml_to_sqlrow(xml):
+def get_title(record):
+    arXiv_prefix = '{http://arxiv.org/OAI/arXiv/}'
+    metadata = record.find('metadata').find(arXiv_prefix + "arXiv")
+    title = metadata.find(arXiv_prefix + "title").text
+    return title
+
+def get_authors(record):
+    arXiv_prefix = '{http://arxiv.org/OAI/arXiv/}'
+    metadata = record.find('metadata').find(arXiv_prefix + "arXiv")
+    authors = metadata.find(arXiv_prefix + 'authors')
+    ls = []
+    for a in authors.getchildren():
+        keyname = a.find(arXiv_prefix + "keyname").text
+        forenames = a.find(arXiv_prefix + "forenames").text
+        ls.append( ' '.join( (forenames, keyname) ) )
+    return ls
+
+def get_categories(record):
+    arXiv_prefix = '{http://arxiv.org/OAI/arXiv/}'
+    metadata = record.find('metadata').find(arXiv_prefix + "arXiv")
+    categories = metadata.find(arXiv_prefix + 'categories').text
+    return categories.split()
+
+def insert_record(record):
+    arXiv_prefix = '{http://arxiv.org/OAI/arXiv/}'
+    
+    article = Article()
+    
+    metadata = record.find('metadata').find(arXiv_prefix + "arXiv")
+    if metadata is None:
+        return 'deleted'
+    
+    identifier = metadata.find(arXiv_prefix + "id").text
+    exists = db.session.query(Article).filter(
+            Article.identifier == identifier
+            ).exists()
+    if db.session.query(exists).scalar():
+        return 'exists'
+    
+    submitted = metadata.find(arXiv_prefix + "created").text
+    submitted = datetime.strptime(submitted, "%Y-%m-%d")
+    title = metadata.find(arXiv_prefix + "title").text
+    abstract = metadata.find(arXiv_prefix + "abstract").text.strip()
+    article.submitted = submitted
+    article.title = title
+    article.identifier = identifier
+    article.abstract = abstract
+
+    authors = metadata.find(arXiv_prefix + 'authors')
+    added_authors = []
+
+    for a in authors.getchildren():
+        keyname = a.find(arXiv_prefix + "keyname")
+        forenames = a.find(arXiv_prefix + "forenames")
+        if keyname is None or forenames is None:
+            continue #bad entry
+        fullname = ' '.join( (forenames.text, keyname.text) )
+        # authors added in this record. Fix error from ['Wende Liu', 'Wende Liu']
+        if fullname in added_authors:
+            continue
+        else:
+            added_authors.append(fullname)
+        search = db.session.query(Author).filter_by(name=fullname)
+        if db.session.query(search.exists()).scalar():
+            article.authors.append(search.one())
+        else:
+            article.authors.append(Author(fullname))
+
+    categories = metadata.find(arXiv_prefix + "categories").text
+    for c in categories.split():
+        search = db.session.query(Category).filter_by(name=c)
+        if db.session.query(search.exists()).scalar():
+            article.categories.append(search.one())
+        else:
+            article.categories.append(Category(c))
+
+    db.session.add(article)
+    return 'inserted'
+
+def insert_xml(xml):
     """
     xml: ElementTree such that xml.getroot() returns an Element
     """
     root = xml.getroot()
-    arXiv_prefix = '{http://arxiv.org/OAI/arXiv/}'
 
-    for record in root.find('ListRecords').findall("record"):        
-        metadata = record.find('metadata').find(arXiv_prefix + "arXiv")
-        submitted = metadata.find(arXiv_prefix + "created").text
-        submitted = datetime.strptime(submitted, "%Y-%m-%d")
-        authors = metadata.find(arXiv_prefix + 'authors').text
-        categories = metadata.find(arXiv_prefix + "categories").text
-        title = metadata.find(arXiv_prefix + "title").text
-        identifier = metadata.find(arXiv_prefix + "id").text
-        abstract = metadata.find(arXiv_prefix + "abstract").text.strip()
-
-        article = Article(
-                identifier = identifier,
-                title = title,
-                authors = authors,
-                abstract = abstract,
-                categories = categories,
-                submitted = submitted
-                )
-        db.session.add(article)
-                
+    for record in root.find('ListRecords').findall("record"):
+        result = insert_record(record)
+        if result == 'exists':
+            print('Skipping file as content already exist.')
+            return
+    db.session.commit() #commit after each file -> a logical unit of work
+    print('Added. Commit.')
 
 if __name__=='__main__':
-    # clear table
-    # Article.query.delete()
+    # if starting from scratch
+    #db.metadata.drop_all(db.engine)
+    #db.metadata.create_all(db.engine)
 
-    files = glob.glob('/home/atkm/.metha/20170923/2017-08-31-*.xml.gz') # test. Use '.metha/20170923/*.xml.gz'.
+    files = glob.glob('/home/atkm/.metha/20170923/*.xml.gz')
+    #files = glob.glob('/home/atkm/.metha/20170923/2017-08-31-*.xml.gz') # test.
 
     try:
-        with gzip.open(files[0], 'rb') as f:
-            xml = ET.parse(f)
-        xml_to_sqlrow(xml)
-        db.session.commit()
-        print('Population succeeded')
+        for xmlgz in files:
+            print(os.path.basename(xmlgz), end=' ')
+            with gzip.open(xmlgz, 'rb') as f:
+                xml = ET.parse(f)
+                insert_xml(xml)
+        print('Population succeeded.')
+        print('%s articles in db.' %
+                db.session.query(Article).count())
     except Exception as e:
+        print('Error in %s.' % xmlgz)
+        print('Rolling back...')
         db.session.rollback()
-        print(e)
+        raise e
--- models.py
+++ models.py
@@ -1,25 +1,57 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
+article_category = db.Table('article_category',
+        db.Column('category_id', db.Integer, db.ForeignKey('categories.id')),
+        db.Column('article_id', db.Integer, db.ForeignKey('articles.id'))
+        )
+
+article_author = db.Table('article_author',
+        db.Column('author_id', db.Integer, db.ForeignKey('authors.id')),
+        db.Column('article_id', db.Integer, db.ForeignKey('articles.id'))
+        )
 
 class Article(db.Model):
-    __tablename__='arxiv_project'
+    __tablename__='articles'
     id = db.Column(db.Integer, primary_key=True)
-    identifier = db.Column(db.String, unique=True)
-    title = db.Column(db.String)
-    authors = db.Column(db.String) # article has-many authors
-    abstract = db.Column(db.String)
-    categories = db.Column(db.String) # article has-many categories
-    submitted = db.Column(db.Date)
+    identifier = db.Column(db.String, unique=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    authors = db.relationship('Author', secondary=article_author,\
+            backref='articles', lazy='dynamic') # backref furnishes Author with .articles
+    abstract = db.Column(db.Text, nullable=False)
+    categories = db.relationship('Category', secondary=article_category,\
+            backref='articles', lazy='dynamic')
+    submitted = db.Column(db.Date, nullable=False)
 
-    def __init__(self, identifier, title,
-            authors, abstract, categories, submitted):
-        self.identifier = identifier
-        self.title = title
-        self.authors = authors
-        self.abstract = abstract
-        self.categories = categories
-        self.submitted = submitted
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<article {}>'.format(self.identifier)
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    # not needed when using backref.
+    #articles = db.relationship('Article', secondary=article_author,\
+    #        back_populates='authors')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<author {}>'.format(self.name)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    #articles = db.relationship('Article', secondary=article_category,\
+    #        back_populates='categories')
+
+    def __init__(self, category):
+        self.name = category
+    
+    def __repr__(self):
+        return '<category {}>'.format(self.name)
